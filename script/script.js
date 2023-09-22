051111eeda8111cc046de84dbd35221f8a6e15db
subin <pc666666@naver.com>
2023-09-22 18:15:12 +0900

    상세페이지js다시

--- script/script.js
+++ script/script.js
@@ -3,12 +3,10 @@ const sns_popup = document.querySelector('#sns_popup')
 const sns_a = document.querySelectorAll('#project_design_sns_design a')
 const sns_popup_img = document.querySelector('#sns_popup img')
 
-sns_popup.style.display = 'none'
-
 sns_a.forEach(function(t,i,a){
     console.log(t,i,a)
     t.addEventListener('click',function(e){
-        e.preventDefault()
+        e.preventDefault();
         sns_popup.style.display = 'block'
         sns_popup_img.src = `./images/design/sns/sns${i+1}.jpg`
     })
